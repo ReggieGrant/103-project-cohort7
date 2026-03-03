e16db07e37012e0b0b5f5787e2fe68c6b739dd08
ReggieGrant <grantreggie09@gmail.com>
2026-03-02 17:26:24 -0800

    Initial Commit

---
+++ 103-project-cohort7/_03_project_cohort7App.swift
@@ -0,0 +1,17 @@
+//
+//  _03_project_cohort7App.swift
+//  103-project-cohort7
+//
+//  Created by Reginald Grant on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct _03_project_cohort7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
